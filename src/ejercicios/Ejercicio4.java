67e7af433d48d5e39f02a91f98d06a9a573a0567
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-05 16:41:31 -0400

    Se crea ejercicio 4, para trabajo practico

--- src/ejercicios/Ejercicio4.java
+++ src/ejercicios/Ejercicio4.java
@@ -2,6 +2,19 @@ package ejercicios;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        
+        int a = 30;
+        boolean nPrimo = true;
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                nPrimo = false;
+                break;
+            }
+        }
+        //Resultado
+        if (nPrimo) {
+            System.out.println(a + " es un número primo");
+        } else {
+            System.out.println(a + " no es un número primo");
+        }
     }
 }
