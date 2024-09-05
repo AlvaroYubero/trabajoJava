4915af34745dfa7d35b20ef3a86201568b60b383
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-05 16:24:11 -0400

    Se crea ejercicio 3, para trabajo practico

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -2,6 +2,11 @@ package ejercicios;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        
+        int a = 15;
+        if (a%2 == 0) {
+            System.out.println(a + " es un número par.");
+        } else {
+            System.out.println(a + " es un número impar.");
+        }
     }
 }
