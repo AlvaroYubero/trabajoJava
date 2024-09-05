55a9527f09e05335d74b46259f8ae518cfca97d2
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-05 17:15:32 -0400

    Se crea ejercicio 6, para trabajo practico

--- src/ejercicios/Ejercicio6.java
+++ src/ejercicios/Ejercicio6.java
@@ -2,6 +2,14 @@ package ejercicios;
 
 public class Ejercicio6 {
     public static void main(String[] args) {
-        
+        int a = 4;
+        int b = 9;
+        int c = 13;
+        // Suma de los tres números.
+        int suma = a + b + c;
+        // Cálculo de Promedio (suma dividido entre la cantidad de números)
+        double promedio = (double) suma / 3;
+        //Resultado.
+        System.out.println("El promedio de " + a + ", " + b + " y " + c + " es: " + promedio);
     }
 }
