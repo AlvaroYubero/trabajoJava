87ce60a5fad97176423a832435814c958aff7091
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-05 17:11:28 -0400

    Se crea ejercicio 5, para trabajo practico

--- src/ejercicios/Ejercicio5.java
+++ src/ejercicios/Ejercicio5.java
@@ -2,6 +2,20 @@ package ejercicios;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-        
+        int a = 5;
+        int b = 12;
+        int c = 9;
+        int maximo;
+
+        // Buscando el número mayor
+        if (a >= b && a >= c) {
+            maximo = a;
+        } else if (b >= a && b >= c) {
+            maximo = b;
+        } else {
+            maximo = c;
+        }
+
+        System.out.println("El número mayor es: " + maximo);
     }
 }
