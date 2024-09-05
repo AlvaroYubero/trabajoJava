6226cf75bcb8bd34280d51bfdf0253a79b0d7b07
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-05 17:11:35 -0400

    Se crea ejercicio 5, para trabajo practico

--- src/ejercicios/Ejercicio4.java
+++ src/ejercicios/Ejercicio4.java
@@ -2,7 +2,7 @@ package ejercicios;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        int a = 30;
+        int a = 29;
         boolean nPrimo = true;
         for (int i = 2; i <= Math.sqrt(a); i++) {
             if (a % i == 0) {
