f584ce8bc0aef069589d67e3a83b3a210d235626
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 13:58:36 -0400

    Se crea ejercicio 11, para trabajo practico

--- src/ejercicios/Ejercicio11.java
+++ src/ejercicios/Ejercicio11.java
@@ -2,6 +2,16 @@ package ejercicios;
 
 public class Ejercicio11 {
     public static void main(String[] args) {
-
+        int N = 10;
+        int num1 = 0, num2 = 1;
+        //Resultado:
+        System.out.print("Los primeros " + N + " números de Fibonacci son: ");
+        for (int i = 1; i <= N; ++i) {
+            System.out.print(num1 + " ");
+            // Actualiza los numeros para la siguiente iteracion
+            int temp = num1 + num2;
+            num1 = num2;
+            num2 = temp;
+        }
     }
 }
