4aed539ee9ddaed3561c1b5e0fd16dfa97f8366c
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 15:17:44 -0400

    Se crea ejercicio 7, para trabajo practico

--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -2,6 +2,11 @@ package ejercicios;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
-        
+        double b = 5;
+        double h = 8;
+        // Cálculo del área: (base * altura) / 2
+        double area = (b * h) / 2;
+        // Resultado.
+        System.out.println("El área del triángulo es: " + area);
     }
 }
