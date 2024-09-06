7c89368802094732d081d096ca767f3d1912f255
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 16:05:34 -0400

    Se crea ejercicio 2, para trabajo practico

--- src/ejercicios/Ejercicio2.java
+++ src/ejercicios/Ejercicio2.java
@@ -5,10 +5,7 @@ public class Ejercicio2 {
         int a = 7;
         int b = 8;
         int resultado = a*b;
-
         System.out.println ("El resultado de: " + a + " y " + b + " es " + resultado);
-
     }
-
 }
 
