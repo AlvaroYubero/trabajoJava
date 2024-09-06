4bcb1788db31ca68a8fd2acb66f0a316a8e5a38d
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 14:44:13 -0400

    Se crea ejercicio 13, para trabajo practico

--- src/ejercicios/Ejercicio13.java
+++ src/ejercicios/Ejercicio13.java
@@ -2,6 +2,21 @@ package ejercicios;
 
 public class Ejercicio13 {
     public static void main(String[] args) {
+        int a = 1221;
+        int numeroOriginal = a;
+        int numeroInvertido = 0;
 
+        // Inversión del número
+        while (a != 0) {
+            int digito = a % 10;
+            numeroInvertido = numeroInvertido * 10 + digito;
+            a /= 10;
+        }
+        // Comparamos el número original con el invertido
+        if (numeroOriginal == numeroInvertido) {
+            System.out.println(numeroOriginal + " es un palíndromo.");
+        } else {
+            System.out.println(numeroOriginal + " no es un palíndromo.");
+        }
     }
 }
