a753b7c454d02ee202b591e8427ec52a6250e5a8
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 15:00:59 -0400

    Se crea ejercicio 14, para trabajo practico

--- src/ejercicios/Ejercicio14.java
+++ src/ejercicios/Ejercicio14.java
@@ -2,6 +2,15 @@ package ejercicios;
 
 public class Ejercicio14 {
     public static void main(String[] args) {
-
+        int a = 45;
+        String binario = "";
+        //Convierte.
+        while (a > 0) {
+            int b = a % 2;
+            binario = b + binario;
+            a /= 2;
+        }
+        //Muestra el resultado.
+        System.out.println("El número decimal " + a + " en binario es: " + binario);
     }
 }
