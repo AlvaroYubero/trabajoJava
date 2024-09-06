77083c23b59cd4516fe7d22de8a65c15e04f0131
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 15:09:11 -0400

    Se crea ejercicio 16, para trabajo practico

--- src/ejercicios/Ejercicio16.java
+++ src/ejercicios/Ejercicio16.java
@@ -2,6 +2,13 @@ package ejercicios;
 
 public class Ejercicio16 {
     public static void main(String[] args) {
-
+        int a = 5678;
+        int suma = 0;
+        while (a != 0) {
+            int digito = a % 10; // Obtenemos el último dígito
+            suma += digito; // Sumamos el dígito a la suma total
+            a /= 10; // Eliminamos el último dígito del número
+        }
+        System.out.println("La suma de los dígitos de " + a + " es: " + suma);
     }
 }
