f742eb4f3e0da10673aeb21677389d1725dcb115
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 15:05:19 -0400

    Se crea ejercicio 15, para trabajo practico

--- src/ejercicios/Ejercicio15.java
+++ src/ejercicios/Ejercicio15.java
@@ -2,6 +2,11 @@ package ejercicios;
 
 public class Ejercicio15 {
     public static void main(String[] args) {
-
+        double radio = 7; // Radio del círculo.
+        double pi = Math.PI;
+        //Calcula.
+        double area = pi * Math.pow(radio, 2);
+        //Resultado.
+        System.out.println("El área del círculo con radio " + radio + " es de: " + area);
     }
 }
