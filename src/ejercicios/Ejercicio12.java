291958e3229d3d05408898d495a341d0764ce4a3
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 14:32:42 -0400

    Se crea ejercicio 12, para trabajo practico

--- src/ejercicios/Ejercicio12.java
+++ src/ejercicios/Ejercicio12.java
@@ -2,6 +2,15 @@ package ejercicios;
 
 public class Ejercicio12 {
     public static void main(String[] args) {
+        int a = 54321;
+        int numeroInvertido = 0;
 
+        while (a != 0) {
+            int digito = a % 10; // Obtiene el ultimo digito.
+            numeroInvertido = numeroInvertido * 10 + digito; // Agrega el digito al numero invertido.
+            a /= 10; // Elimina el ultimo digito del numero original.
+        }
+        //Muestra el numero invertido.
+        System.out.println("El número invertido es: " + numeroInvertido);
     }
 }
