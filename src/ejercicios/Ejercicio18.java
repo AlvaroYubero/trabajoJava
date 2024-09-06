ff3468c02e7d0adaa363819a5e928e96b659dd69
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 16:15:47 -0400

    Se crea ejercicio 18, para trabajo practico

--- src/ejercicios/Ejercicio18.java
+++ src/ejercicios/Ejercicio18.java
@@ -2,6 +2,13 @@ package ejercicios;
 
 public class Ejercicio18 {
     public static void main(String[] args) {
-
+        int a = 2;
+        int b = 8;
+        int resultado = 1;
+        // Multiplicar la base por sí misma el número de veces indicado por el exponente
+        for (int i = 0; i < b; i++) {
+            resultado *= a;
+        }
+        System.out.println(a + " elevado a " + b + " es: " + resultado);
     }
 }
