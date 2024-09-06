f51d6765658eedb3749d603a99686d7e7461a6af
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 16:10:30 -0400

    Se crea ejercicio 17, para trabajo practico

--- src/ejercicios/Ejercicio17.java
+++ src/ejercicios/Ejercicio17.java
@@ -2,6 +2,14 @@ package ejercicios;
 
 public class Ejercicio17 {
     public static void main(String[] args) {
-
+        int a = 1;
+        int b = 100;
+        int sumaPares = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0) {
+                sumaPares += i;
+            }
+        }
+        System.out.println("La suma de números pares entre " + a + " y " + b + " es: " + sumaPares);
     }
 }
