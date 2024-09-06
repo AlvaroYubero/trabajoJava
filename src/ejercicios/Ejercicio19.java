ba610f29bcbfc17ae93fb0a63848432c8ea8f49a
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 16:31:44 -0400

    Se crea ejercicio 19, para trabajo practico

--- src/ejercicios/Ejercicio19.java
+++ src/ejercicios/Ejercicio19.java
@@ -2,6 +2,19 @@ package ejercicios;
 
 public class Ejercicio19 {
     public static void main(String[] args) {
-
+        int a = 28;
+        int sumaDivisores = 0;
+        // Iteramos desde 1 hasta la mitad del número
+        for (int i = 1; i <= a / 2; i++) {
+            if (a % i == 0) {
+                sumaDivisores += i;
+            }
+        }
+        // Se verifica si la suma de los divisores es igual al número
+        if (sumaDivisores == a) {
+            System.out.println(a + " es un número perfecto.");
+        } else {
+            System.out.println(a + " no es un número perfecto.");
+        }
     }
 }
