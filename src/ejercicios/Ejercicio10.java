2d5db2b627e51060a139d3dbba821784e5dbb464
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 13:54:29 -0400

    Se crea ejercicio 9, para trabajo practico

--- src/ejercicios/Ejercicio10.java
+++ src/ejercicios/Ejercicio10.java
@@ -2,6 +2,12 @@ package ejercicios;
 
 public class Ejercicio10 {
     public static void main(String[] args) {
-
+        int a = 6;
+        int factorial = 1;
+        for (int i = 1; i <= a; i++) {
+            factorial *= i;
+        }
+        //Resultado:
+        System.out.println("El factorial de " + a + " es: " + factorial);
     }
 }
