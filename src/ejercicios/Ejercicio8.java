05a0eab65675e8e4bb067158a1c547459681b2f8
alvaro.yubero <alvaro.yy@gmail.com>
2024-09-06 16:03:39 -0400

    Se crea ejercicio 8, para trabajo practico

--- src/ejercicios/Ejercicio8.java
+++ src/ejercicios/Ejercicio8.java
@@ -2,6 +2,10 @@ package ejercicios;
 
 public class Ejercicio8 {
     public static void main(String[] args) {
-        
+        double c = 25;
+        // Conversión: Fahrenheit = (Celsius * 9/5) + 32
+        double f = (c * 9/5) + 32;
+        //Aqui muestra el resultado.
+        System.out.println(c + " grados Celsius equivalen a " + f + " grados Fahrenheit.");
     }
 }
